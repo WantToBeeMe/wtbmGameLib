fa62149255bbf951d49d3287f9212d979a5054f5
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2023-11-29 22:36:51 +0100

    Update WTBM.kt

--- src/main/kotlin/me/wanttobee/wtbmGameLib/WTBM.kt
+++ src/main/kotlin/me/wanttobee/wtbmGameLib/WTBM.kt
@@ -4,12 +4,20 @@ object WTBM {
     var devMode = false
         private set
 
+
     fun setDevMode(enable: Boolean){
         devMode = enable
         if(enable)
             Logger.setLogLevel(Logger.LogLevel.DEBUG)
         else
             Logger.setLogLevel(Logger.LogLevel.ERROR)
+    }
+
+    fun recommendedMainArgsConfig(args: Array<String>){
+        if(args.contains("-dev"))
+            setDevMode(true)
+        if(args.contains("-log"))
+            Logger.setLogLevel(Logger.LogLevel.INFO)
     }
 
 }
